d539c9054cb77eb5efd03f28f5a9e7e6b87677de
Heathcliff <dennis.nathan@gmail.com>
2019-01-31 13:12:56 -0700

    When recipe is clicked, its specific information is displayed.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,13 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://placekitten.com/200/300')
+    new Recipe('A Test Recipe', 'This is a test', 'https://placekitten.com/200/300'),
+    new Recipe('Another Test Recipe', 'This is a test', 'https://placekitten.com/200/300')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
