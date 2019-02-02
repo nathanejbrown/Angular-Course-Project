db89495e390c459d82a892c64e386d052b242dcf
Heathcliff <dennis.nathan@gmail.com>
2019-02-02 11:29:33 -0700

    Setup recipe service to share recipe data between all components.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a test', 'https://placekitten.com/200/300'),
         new Recipe('Another Test Recipe', 'This is a test', 'https://placekitten.com/200/300')
