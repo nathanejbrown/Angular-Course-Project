e7869f67253e63c5b648221d7b6bd7733d777b68
Heathcliff <dennis.nathan@gmail.com>
2019-02-02 12:56:00 -0700

    Began setting up shopping-list service to handle the data in the shopping list components.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,7 +2,7 @@ import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
-    recipeSelected = new EventEmitter
+    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a test', 'https://placekitten.com/200/300'),
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
-export class ShoppingList {
-    
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService {
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Potatoes', 10)
+      ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
 }
