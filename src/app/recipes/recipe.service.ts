0a4c7842c083ede3babd487221b5f397eb1883c1
Heathcliff <dennis.nathan@gmail.com>
2019-02-02 13:12:10 -0700

    Edited recipes to hold an array of ingredients.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://placekitten.com/200/300'),
-        new Recipe('Another Test Recipe', 'This is a test', 'https://placekitten.com/200/300')
+        new Recipe(
+            'A Test Recipe', 
+            'This is a test', 
+            'https://placekitten.com/200/300',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Another Test Recipe', 
+            'This is a test', 
+            'https://placekitten.com/200/300',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
       ];
 
     getRecipes() {
